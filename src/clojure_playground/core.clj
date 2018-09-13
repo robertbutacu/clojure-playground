23a87f490b0466c930ebe6d764dbb81ae60561df
robertbutacu <robertbutacu@gmail.com>
2018-09-13 11:16:48 +0100

    getting familiar with data structures and function calling

--- src/clojure_playground/core.clj
+++ src/clojure_playground/core.clj
@@ -4,3 +4,11 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+;comment
+(foo ())
+[1 2 3]                                                      ; vector
+'(1 2 3)                                                     ; linked list
+#{:1 :2 :3}                                                  ; set
+{:1 "one" :2 "two" :3 "three"}                               ; dictionary
